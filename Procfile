ab564fc4ddc415e03d1d8b1a193287d2d99a1c04
Matt Layman <matthewlayman@gmail.com>
2019-12-04 22:39:50 -0500

    Make `heroku local` work.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn project.wsgi --log-file -
--- project/settings.py
+++ project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -112,3 +114,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+
+django_heroku.settings(locals())
