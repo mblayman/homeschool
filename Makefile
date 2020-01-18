b7664c2c2399d2567175287d559670ca6d7a75b4
Matt Layman <matthewlayman@gmail.com>
2020-01-18 16:30:30 -0500

    Add ability to generate modeling diagram.

--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@
 
 local:
 	heroku local -f Procfile.local
+
+graph:
+	./manage.py graph_models core courses schools users -o models.png
--- project/settings.py
+++ project/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
+    "django_extensions",
     "waffle",
     "homeschool.core",
     "homeschool.courses",
