080971b351f3f2dfe58470379f665ddb231ed08b
Matt Layman <matthewlayman@gmail.com>
2020-01-20 21:26:03 -0500

    Finish the fetching command.

--- homeschool/core/management/commands/fetch_skedtrack.py
+++ homeschool/core/management/commands/fetch_skedtrack.py
@@ -1,4 +1,6 @@
+import io
 import os
+import time
 
 import requests
 from bs4 import BeautifulSoup
@@ -7,25 +9,21 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     help = "Fetch courses from Homeschool Skedtrack"
+    url = "http://www.homeschoolskedtrack.com/HomeSchool"
 
     def handle(self, *args, **kwargs):
-        session = self.get_authenticated_session()
-        self.get_download_ids(session)
+        self.get_authenticated_session()
+        ids = self.get_download_ids()
 
-    def get_download_ids(self, session):
-        display_downloads_url = (
-            "http://www.homeschoolskedtrack.com/HomeSchool/displayDownloads.do"
-        )
-        # response = session.get(display_downloads_url)
-        # response_text = response.text
-        # with open("downloads.html", "w") as f:
-        #     f.write(response_text)
-        with open("downloads.html", "r") as f:
-            response_text = f.read()
-        soup = BeautifulSoup(response_text, "html.parser")
-        inputs = soup.find_all("input", attrs={"name": "download"})
-        for input_tag in inputs:
-            print(input_tag["value"])
+        # TODO: Select student.
+
+        out_dir = "courses_exports"
+        if not os.path.exists(out_dir):
+            os.mkdir(out_dir)
+
+        for course_id in ids:
+            time.sleep(5)
+            self.fetch_course(course_id, out_dir)
 
     def get_authenticated_session(self):
         """Get a session to use to communicate with Homeschool Skedtrack."""
@@ -35,6 +33,42 @@ class Command(BaseCommand):
 
         session = requests.Session()
 
-        login_url = "http://www.homeschoolskedtrack.com/HomeSchool/login.do"
-        # session.post(login_url, data={"userName": username, "password": password})
-        return session
+        login_url = f"{self.url}/login.do"
+        session.post(login_url, data={"userName": username, "password": password})
+        self.session = session
+
+    def get_download_ids(self):
+        self.stdout.write("Get download ids")
+        display_downloads_url = f"{self.url}/displayDownloads.do"
+        response = self.session.get(display_downloads_url)
+        response_text = response.text
+
+        # with open("downloads.html", "w") as f:
+        #     f.write(response_text)
+        # with open("downloads.html", "r") as f:
+        #     response_text = f.read()
+
+        soup = BeautifulSoup(response_text, "html.parser")
+
+        # Yeah, it's a side effect. Whatever.
+        enrollment_option = soup.find(id="enrollmentId").find("option", selected=True)
+        self.enrollment_id = enrollment_option["value"]
+
+        inputs = soup.find_all("input", attrs={"name": "download"})
+        return [input_tag["value"] for input_tag in inputs]
+
+    def fetch_course(self, course_id, out_dir):
+        """Fetch a course and write it to a CSV file."""
+        self.stdout.write(f"Fetch course data for {course_id}")
+        download_url = f"{self.url}/download.do?fileType=csv"
+        response = self.session.post(
+            download_url,
+            data={"download": course_id, "enrollmentId": self.enrollment_id},
+        )
+
+        csv_data = io.StringIO(response.text)
+        course_name = csv_data.readline().split(":")[1].strip()
+        csv_file_name = f"{out_dir}/{course_name}.csv"
+        self.stdout.write(f"Writing out CSV at {csv_file_name}")
+        with open(csv_file_name, "w") as f:
+            f.write(response.text)
