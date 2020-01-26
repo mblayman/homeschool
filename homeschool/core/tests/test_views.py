42d236001d5ee8bedca05b5d144c9fe707a1ff81
Matt Layman <matthewlayman@gmail.com>
2020-01-26 15:16:19 -0500

    Set the week boundaries.

--- homeschool/core/tests/test_views.py
+++ homeschool/core/tests/test_views.py
@@ -1,3 +1,9 @@
+import datetime
+from unittest import mock
+
+import pytz
+from dateutil.relativedelta import MO, SU, relativedelta
+
 from homeschool.test import TestCase
 
 
@@ -9,8 +15,33 @@ class TestIndex(TestCase):
 class TestApp(TestCase):
     def test_ok(self):
         user = self.make_user()
+
         with self.login(user):
             self.get_check_200("core:app")
 
     def test_unauthenticated_access(self):
         self.assertLoginRequired("core:app")
+
+    @mock.patch("homeschool.core.views.timezone")
+    def test_has_monday(self, timezone):
+        user = self.make_user()
+        now = datetime.datetime(2020, 1, 26, tzinfo=pytz.utc)
+        monday = now.date() + relativedelta(weekday=MO(-1))
+        timezone.now.return_value = now
+
+        with self.login(user):
+            self.get("core:app")
+
+        self.assertContext("monday", monday)
+
+    @mock.patch("homeschool.core.views.timezone")
+    def test_has_sunday(self, timezone):
+        user = self.make_user()
+        now = datetime.datetime(2020, 1, 26, tzinfo=pytz.utc)
+        sunday = now.date() + relativedelta(weekday=SU(+1))
+        timezone.now.return_value = now
+
+        with self.login(user):
+            self.get("core:app")
+
+        self.assertContext("sunday", sunday)
--- homeschool/core/views.py
+++ homeschool/core/views.py
@@ -1,4 +1,6 @@
+from dateutil.relativedelta import MO, SU, relativedelta
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils import timezone
 from django.views.generic.base import TemplateView
 
 
@@ -8,3 +10,16 @@ class IndexView(TemplateView):
 
 class AppView(LoginRequiredMixin, TemplateView):
     template_name = "core/app.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        self.set_week_boundaries(context)
+        return context
+
+    def set_week_boundaries(self, context):
+        """Set the Monday and Sunday that bound today."""
+        today = timezone.now().date()
+        monday = today + relativedelta(weekday=MO(-1))
+        context["monday"] = monday
+        sunday = today + relativedelta(weekday=SU(+1))
+        context["sunday"] = sunday
