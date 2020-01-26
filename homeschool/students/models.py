e30a661c73bd10702006e3e812d5728bc33afb0c
Matt Layman <matthewlayman@gmail.com>
2020-01-26 09:40:29 -0500

    Add db_index to completed_date.

--- homeschool/students/models.py
+++ homeschool/students/models.py
@@ -31,4 +31,4 @@ class Coursework(models.Model):
 
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     course_task = models.ForeignKey("courses.CourseTask", on_delete=models.CASCADE)
-    completed_date = models.DateField()
+    completed_date = models.DateField(db_index=True)
