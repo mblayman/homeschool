78a5e4f74b2edebeee3bb1fa86ec8230c36668aa
Matt Layman <matthewlayman@gmail.com>
2020-01-28 00:09:30 -0500

    Add context for schedule for student for main app view.

--- homeschool/core/tests/test_views.py
+++ homeschool/core/tests/test_views.py
@@ -4,9 +4,14 @@ from unittest import mock
 import pytz
 from dateutil.relativedelta import MO, SU, relativedelta
 
-from homeschool.courses.tests.factories import CourseFactory
+from homeschool.courses.models import Course
+from homeschool.courses.tests.factories import CourseFactory, CourseTaskFactory
 from homeschool.schools.tests.factories import GradeLevelFactory, SchoolYearFactory
-from homeschool.students.tests.factories import EnrollmentFactory, StudentFactory
+from homeschool.students.tests.factories import (
+    CourseworkFactory,
+    EnrollmentFactory,
+    StudentFactory,
+)
 from homeschool.test import TestCase
 
 
@@ -49,18 +54,53 @@ class TestApp(TestCase):
 
         self.assertContext("sunday", sunday)
 
-    def test_has_schedules(self):
+    @mock.patch("homeschool.core.views.timezone")
+    def test_has_schedules(self, timezone):
+        now = datetime.datetime(2020, 1, 26, tzinfo=pytz.utc)
+        sunday = now.date()
+        monday = sunday - datetime.timedelta(days=6)
+        timezone.now.return_value = now
         user = self.make_user()
         student = StudentFactory(school=user.school)
-        school_year = SchoolYearFactory(school=user.school)
+        school_year = SchoolYearFactory(
+            school=user.school,
+            start_date=sunday - datetime.timedelta(days=90),
+            end_date=sunday + datetime.timedelta(days=90),
+        )
         grade_level = GradeLevelFactory(school_year=school_year)
-        course = CourseFactory(grade_level=grade_level)
+        course = CourseFactory(
+            grade_level=grade_level,
+            days_of_week=Course.MONDAY
+            + Course.WEDNESDAY
+            + Course.THURSDAY
+            + Course.FRIDAY,
+        )
+        task_1 = CourseTaskFactory(course=course)
+        task_2 = CourseTaskFactory(course=course)
+        task_3 = CourseTaskFactory(course=course)
+        coursework = CourseworkFactory(
+            student=student, course_task=task_1, completed_date=monday
+        )
         EnrollmentFactory(student=student, grade_level=grade_level)
 
-        with self.login(user), self.assertNumQueries(8):
+        with self.login(user), self.assertNumQueries(11):
             self.get("core:app")
 
-        expected_schedule = {"student": student, "courses": [course]}
+        expected_schedule = {
+            "student": student,
+            "courses": [
+                {
+                    "course": course,
+                    "days": [
+                        {"coursework": [coursework]},
+                        None,
+                        {"task": task_2},
+                        {"task": task_3},
+                        None,
+                    ],
+                }
+            ],
+        }
         self.assertContext("schedules", [expected_schedule])
 
     def test_no_school_year(self):
--- homeschool/core/views.py
+++ homeschool/core/views.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 from django.views.generic.base import TemplateView
 
 from homeschool.schools.models import SchoolYear
+from homeschool.students.models import Coursework
 
 
 class IndexView(TemplateView):
@@ -16,7 +17,8 @@ class AppView(LoginRequiredMixin, TemplateView):
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
         today = timezone.now().date()
-        context["monday"], context["sunday"] = self.get_week_boundaries(today)
+        week = self.get_week_boundaries(today)
+        context["monday"], context["sunday"] = week
 
         school_year = (
             SchoolYear.objects.filter(start_date__lte=today, end_date__gte=today)
@@ -24,9 +26,7 @@ class AppView(LoginRequiredMixin, TemplateView):
             .first()
         )
 
-        context["schedules"] = self.get_schedules(
-            school_year, context["monday"], context["sunday"]
-        )
+        context["schedules"] = self.get_schedules(school_year, week)
         return context
 
     def get_week_boundaries(self, today):
@@ -35,15 +35,82 @@ class AppView(LoginRequiredMixin, TemplateView):
         sunday = today + relativedelta(weekday=SU(+1))
         return monday, sunday
 
-    def get_schedules(self, school_year, monday, sunday):
+    def get_schedules(self, school_year, week):
         """Get the schedules for each student."""
         schedules = []
         if school_year is None:
             return schedules
 
+        week_dates = school_year.get_week_dates_for(week)
         for student in self.request.user.school.students.all():
             courses = student.get_courses(school_year)
-            schedule = {"student": student, "courses": courses}
+            week_coursework = self.get_student_week_coursework(student, courses, week)
+            schedule = self.get_student_schedule(
+                student, week_dates, courses, week_coursework
+            )
             schedules.append(schedule)
 
         return schedules
+
+    def get_student_week_coursework(self, student, courses, week):
+        """Get the student's week coursework in dict for fast lookup."""
+        week_coursework = {}
+        coursework_qs = Coursework.objects.filter(
+            student=student, course_task__course__in=courses, completed_date__range=week
+        ).select_related("course_task")
+        for coursework in coursework_qs:
+            course_id = coursework.course_task.course_id
+            if course_id not in week_coursework:
+                week_coursework[course_id] = {}
+
+            if coursework.completed_date not in week_coursework[course_id]:
+                # It's possible for multiple coursework items to share the same
+                # completion day because that's controlled by user input.
+                week_coursework[course_id][coursework.completed_date] = []
+
+            week_coursework[course_id][coursework.completed_date].append(coursework)
+
+        return week_coursework
+
+    def get_student_schedule(self, student, week_dates, courses, week_coursework):
+        """Get the schedule.
+
+        Each student will get a list of courses, filled with each day.
+        Empty slots will contain None.
+        """
+        completed_task_ids = list(
+            Coursework.objects.filter(
+                student=student, course_task__course__in=courses
+            ).values_list("course_task_id", flat=True)
+        )
+        task_limit = len(week_dates)
+        schedule = {"student": student, "courses": []}
+        for course in courses:
+            course_schedule = {"course": course, "days": []}
+            # Doing this query in a loop is definitely an N+1 bug.
+            # If it's possible to do a single query of all tasks
+            # that groups by course then that would be better.
+            # No need to over-optimize until that's a real issue.
+            course_tasks = list(
+                course.course_tasks.exclude(id__in=completed_task_ids)[:task_limit]
+            )
+            course_tasks.reverse()
+            for week_date in week_dates:
+                if (
+                    course.id in week_coursework
+                    and week_date in week_coursework[course.id]
+                ):
+                    coursework_list = week_coursework[course.id][week_date]
+                    course_schedule["days"].append({"coursework": coursework_list})
+                    continue
+
+                if course.runs_on(week_date):
+                    if course_tasks:
+                        course_schedule["days"].append({"task": course_tasks.pop()})
+                    else:
+                        # There may be no tasks left to pull from.
+                        course_schedule["days"].append(None)
+                else:
+                    course_schedule["days"].append(None)
+            schedule["courses"].append(course_schedule)
+        return schedule
