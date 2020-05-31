b2b8e2d49523f2c441ab6009273ae98db1f39fb7
Matt Layman <matthewlayman@gmail.com>
2020-05-30 22:57:24 -0400

    Refactor CourseForm to pull out DaysOfWeekModelForm.
    For #83

---
+++ homeschool/core/forms.py
@@ -0,0 +1,47 @@
+from django import forms
+
+from .models import DaysOfWeekModel
+
+
+class DaysOfWeekModelForm(forms.ModelForm):
+    """A form to handle a DaysOfWeekModel.
+
+    Sub-classing forms must define their own Meta
+    and extend `fields` with days_of_week_fields.
+    """
+
+    monday = forms.BooleanField(required=False)
+    tuesday = forms.BooleanField(required=False)
+    wednesday = forms.BooleanField(required=False)
+    thursday = forms.BooleanField(required=False)
+    friday = forms.BooleanField(required=False)
+    saturday = forms.BooleanField(required=False)
+    sunday = forms.BooleanField(required=False)
+
+    days_of_week_fields = [
+        "monday",
+        "tuesday",
+        "wednesday",
+        "thursday",
+        "friday",
+        "saturday",
+        "sunday",
+    ]
+
+    def save(self, *args, **kwargs):
+        """Save the model instance and the days of week."""
+        days_of_week = 0
+        day_field_to_model_day = {
+            "monday": DaysOfWeekModel.MONDAY,
+            "tuesday": DaysOfWeekModel.TUESDAY,
+            "wednesday": DaysOfWeekModel.WEDNESDAY,
+            "thursday": DaysOfWeekModel.THURSDAY,
+            "friday": DaysOfWeekModel.FRIDAY,
+            "saturday": DaysOfWeekModel.SATURDAY,
+            "sunday": DaysOfWeekModel.SUNDAY,
+        }
+        for day_field, model_day in day_field_to_model_day.items():
+            if self.cleaned_data.get(day_field):
+                days_of_week += model_day
+        self.instance.days_of_week = days_of_week
+        return super().save(*args, **kwargs)
--- homeschool/courses/forms.py
+++ homeschool/courses/forms.py
@@ -1,47 +1,14 @@
 from django import forms
 
+from homeschool.core.forms import DaysOfWeekModelForm
+
 from .models import Course, CourseTask, GradedWork
 
 
-class CourseForm(forms.ModelForm):
-    monday = forms.BooleanField(required=False)
-    tuesday = forms.BooleanField(required=False)
-    wednesday = forms.BooleanField(required=False)
-    thursday = forms.BooleanField(required=False)
-    friday = forms.BooleanField(required=False)
-    saturday = forms.BooleanField(required=False)
-    sunday = forms.BooleanField(required=False)
-
+class CourseForm(DaysOfWeekModelForm):
     class Meta:
         model = Course
-        fields = [
-            "name",
-            "monday",
-            "tuesday",
-            "wednesday",
-            "thursday",
-            "friday",
-            "saturday",
-            "sunday",
-        ]
-
-    def save(self, *args, **kwargs):
-        """Save the course and the days of week."""
-        days_of_week = 0
-        day_to_course_day = {
-            "monday": Course.MONDAY,
-            "tuesday": Course.TUESDAY,
-            "wednesday": Course.WEDNESDAY,
-            "thursday": Course.THURSDAY,
-            "friday": Course.FRIDAY,
-            "saturday": Course.SATURDAY,
-            "sunday": Course.SUNDAY,
-        }
-        for day, course_day in day_to_course_day.items():
-            if self.cleaned_data.get(day):
-                days_of_week += course_day
-        self.instance.days_of_week = days_of_week
-        return super().save(*args, **kwargs)
+        fields = ["name"] + DaysOfWeekModelForm.days_of_week_fields
 
 
 class CourseTaskForm(forms.ModelForm):
