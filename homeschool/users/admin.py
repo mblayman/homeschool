51522a50d8efcca79a05ff9de16950c3ac4afa54
Matt Layman <matthewlayman@gmail.com>
2021-09-01 11:37:58 -0400

    Fix up hijack to use new APIs.

--- homeschool/users/admin.py
+++ homeschool/users/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
-from hijack.helpers import login_user
 
 from .models import Profile
 
@@ -12,14 +11,7 @@ class ProfileInline(admin.StackedInline):
 
 @admin.register(get_user_model())
 class UserAdmin(AuthUserAdmin):
-    actions = ["hijack_user"]
     inlines = [ProfileInline]
-
-    def hijack_user(self, request, queryset):
-        """Hijack a user."""
-        if len(queryset) == 1:
-            user = queryset[0]
-            return login_user(request, user)
 
 
 @admin.register(Profile)
