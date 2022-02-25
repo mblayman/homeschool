5dbdbbc8c447dacaecbbd3636c6f477b3862e064
Matt Layman <matthewlayman@gmail.com>
2022-02-25 00:31:43 -0500

    Put in some stylesheet stuff.

--- homeschool/reports/pdfs.py
+++ homeschool/reports/pdfs.py
@@ -1,8 +1,11 @@
 from dataclasses import asdict
 from io import BytesIO
 
+from django.conf import settings
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.template.loader import render_to_string
-from weasyprint import HTML
+from django.templatetags.static import static
+from weasyprint import CSS, HTML
 
 from .contexts import ResourceReportContext
 
@@ -13,8 +16,11 @@ def make_resource_report(context: ResourceReportContext) -> bytes:
     Return raw PDF data.
     """
     # TODO: Needs tests.
+    site_css = static("site.css")
+    site_css_path = settings.STATIC_ROOT / site_css.replace(settings.STATIC_URL, "")
+    site_css_path = staticfiles_storage.path("site.css")
     rendered = render_to_string("reports/resource_report_pdf.html", asdict(context))
     html = HTML(string=rendered)
     io = BytesIO()
-    html.write_pdf(io)
+    html.write_pdf(io, stylesheets=[CSS(site_css_path)])
     return io.getvalue()
