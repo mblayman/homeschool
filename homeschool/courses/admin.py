0a3843c134339e5030f040d916609c3b430348fa
Matt Layman <matthewlayman@gmail.com>
2024-03-12 22:25:37 -0400

    Add an inline for better debugging of a course.

--- homeschool/courses/admin.py
+++ homeschool/courses/admin.py
@@ -4,14 +4,21 @@ from ordered_model.admin import OrderedModelAdmin
 from homeschool.courses.models import Course, CourseResource, CourseTask, GradedWork
 
 
+class CourseTaskInline(admin.TabularInline):
+    model = CourseTask
+    fields = ["description", "order"]
+    readonly_fields = ["order"]
+
+
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     list_display = ("name",)
+    inlines = [CourseTaskInline]
 
 
 @admin.register(CourseTask)
 class CourseTaskAdmin(OrderedModelAdmin):
-    list_display = ("course", "description", "move_up_down_links")
+    list_display = ("course", "description", "order", "move_up_down_links")
     ordering = ("-id",)
 
 
