5c477ce9e6776b14dee83758682b24bb5fd69146
Matt Layman <matthewlayman@gmail.com>
2026-05-16 22:33:24 -0400

    Apply fix for Django 6 compat.

--- hashid_field/lookups.py
+++ hashid_field/lookups.py
@@ -92,7 +92,7 @@ class HashidExactLookup(HashidFieldGetDbPrepValueMixin, Lookup):
     def as_sql(self, compiler, connection):
         lhs_sql, params = self.process_lhs(compiler, connection)
         rhs_sql, rhs_params = self.process_rhs(compiler, connection)
-        params.extend(rhs_params)
+        params = (*params, *rhs_params)
         rhs_sql = self.get_rhs_op(connection, rhs_sql)
         return f"{lhs_sql} {rhs_sql}", params
 
